d06383e03c67d530045da809430974ee3d62701f
Sondos Hammad <89040514+Sondos-Sabry@users.noreply.github.com>
2023-05-12 02:15:17 +0300

    Update app.js
    adding connection mysql database

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+///connection with mysql database
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'collage_system'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+  } else {
+    console.log('Connected to database!');
+  }
+});
+
 ///for auto refresh
 const path = require("path");
 const livereload = require("livereload");
@@ -8,6 +26,7 @@ const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, 'public'));
  
 const connectLivereload = require("connect-livereload");
+const { error } = require('console')
 app.use(connectLivereload());
  
 liveReloadServer.server.once("connection", () => {
@@ -18,8 +37,6 @@ liveReloadServer.server.once("connection", () => {
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-
-
 
 //viewsبتعامل كان انا واقف داخل فولدر 
 app.get("/", (req, res) => {
@@ -50,6 +67,7 @@ app.get("/add_department", (req, res) => {
 app.get("/add_cource", (req, res) => {
   res.render("add_material")
 });
+
 ///404 error
 app.use((req, res) => {
     res.status(404).send("sorry can't find that!")
